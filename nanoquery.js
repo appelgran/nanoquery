c4831b4c4753280085a17d098b7b38feb66daa5b
appelgran <kolibanat@gmail.com>
2020-01-22 20:01:06 +0100

    Update and change ñ to nano

---
+++ nanoquery.js
@@ -0,0 +1,9 @@
+﻿/*
+    nanoquery function (can replace jQuery in 90% cases) 
+    Syntax:
+    nano(selector) - select and return the first matching element
+    nano(selector, callback) - perform a callback on all selected elements
+    nano(selector, event, handler) - add event handler to all selected elements
+*/
+
+window.nano=window.nanoquery=function(s,c,x){var r=document.querySelectorAll(s);return r.length?(c?[].forEach.call(r,x?function(e){e.addEventListener(c,x,!!0);}:c):r[0]):null;};
