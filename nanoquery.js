3d1ba8272a54f31641c782c8075b0a11df3b80b6
appelgran <patrik@appelgran.org>
2020-11-13 15:47:03 +0100

    Added nanowith

--- nanoquery.js
+++ nanoquery.js
@@ -1,9 +1,11 @@
-﻿/*
-    nanoquery function (can replace jQuery in 90% cases) 
+/*
+    nanoquery
     Syntax:
     nano(selector) - select and return the first matching element
     nano(selector, callback) - perform a callback on all selected elements
     nano(selector, event, handler) - add event handler to all selected elements
+    nanowith(element, ...) - as nano but on element instead of document
 */
 
-window.nano=window.nanoquery=function(s,c,x){var r=document.querySelectorAll(s);return r.length?(c?[].forEach.call(r,x?function(e){e.addEventListener(c,x,!!0);}:c):r[0]):null;};
+window.nanowith=window.nanoquerywith=function(a,s,c,x){var r=a.querySelectorAll(s);return r.length?(c?[].forEach.call(r,x?function(e){e.addEventListener(c,x,!!0);}:c):r[0]):null;};
+window.nano=window.nanoquery=function(s,c,x){return nanoquerywith(document,s,c,x);};
